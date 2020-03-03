50a5a7beaab959efe5edb2c54f16cda74127b7fb
sweeney153 <seansweeney153@gmail.com>
2020-03-02 21:07:56 -0500

    completed tic tac toe and poker app

--- Part2/index.js
+++ Part2/index.js
@@ -1,11 +1,11 @@
-const idInput = document.getElementById('idInput');
-const colorInput = document.getElementById('colorInput');
-
-console.log(idInput);
-console.log(colorInput);
+const idInput = document.getElementById("idInput")
+const colorInput = document.getElementById("colorInput")
 
 function setCard() {
-    const card = document.getElementById(idInput.value);
-    card.style.color = colorInput.value;
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
 }
 
+function reset() {
+  document.getElementById("diamond").reset();
+}
